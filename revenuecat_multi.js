d9b5fd5f6a58ba253b6d50b43a84af01d269ebb8
tranquocvu <tranquocvu3011@gmail.com>
2026-04-25 12:14:23 +0700

    Bypass Locket in RevenueCat mock to prevent breaking real receipts

--- revenuecat_multi.js
+++ revenuecat_multi.js
@@ -12,43 +12,52 @@ const mapping = {
 };
 
 // ========= Core Logic ========= //
-var ua = $request.headers["User-Agent"] || $request.headers["user-agent"],
-  obj = JSON.parse($response.body);
+var ua = $request.headers["User-Agent"] || $request.headers["user-agent"];
 
-var subscriptionData = {
-      auto_resume_date: null,
-      display_name: "locket_1600_1y",
-      is_sandbox: true,
-      ownership_type: "PURCHASED",
-      billing_issues_detected_at: null,
-      management_url: "https://apps.apple.com/account/subscriptions",
-      period_type: "normal",
-      price: {
-          "amount": 399000.0,
-          "currency": "VND"
-      },
-      expires_date: "9999-01-09T10:10:14Z",
-      grace_period_expires_date: null,
-      refunded_at: null,
-      unsubscribe_detected_at: null,
-      original_purchase_date: "2025-10-20T10:10:15Z",
-      purchase_date: "2025-10-20T10:10:14Z",
-      store: "app_store",
-      store_transaction_id: "2000001108724193",
-  },
-  locketGold = {
-      grace_period_expires_date: null,
-      purchase_date: "2025-10-20T10:10:14Z",
-      product_identifier: "locket_1600_1y",
-      expires_date: "9999-01-09T10:10:14Z"
-  };
+// Bỏ qua hoàn toàn Locket để không ghi đè Receipt thật từ server
+if (ua && ua.includes('Locket')) {
+  $done({});
+} else {
+  var obj = JSON.parse($response.body);
 
-const match = Object.keys(mapping).find(e => ua.includes(e));
-if (match) {
-  let [e, s] = mapping[match];
-  s ? (locketGold.product_identifier = s, obj.subscriber.subscriptions[s] = subscriptionData) : obj.subscriber.subscriptions["locket_1600_1y"] = subscriptionData, obj.subscriber.entitlements[e] = locketGold
-} else obj.subscriber.subscriptions["locket_1600_1y"] = subscriptionData, obj.subscriber.entitlements.pro = locketGold;
+  var subscriptionData = {
+        auto_resume_date: null,
+        display_name: "pro_1y",
+        is_sandbox: true,
+        ownership_type: "PURCHASED",
+        billing_issues_detected_at: null,
+        management_url: "https://apps.apple.com/account/subscriptions",
+        period_type: "normal",
+        price: {
+            "amount": 399000.0,
+            "currency": "VND"
+        },
+        expires_date: "9999-01-09T10:10:14Z",
+        grace_period_expires_date: null,
+        refunded_at: null,
+        unsubscribe_detected_at: null,
+        original_purchase_date: "2025-10-20T10:10:15Z",
+        purchase_date: "2025-10-20T10:10:14Z",
+        store: "app_store",
+        store_transaction_id: "2000001108724193",
+    },
+    entitlementData = {
+        grace_period_expires_date: null,
+        purchase_date: "2025-10-20T10:10:14Z",
+        product_identifier: "pro_1y",
+        expires_date: "9999-01-09T10:10:14Z"
+    };
 
-$done({
-  body: JSON.stringify(obj)
-});
+  const match = Object.keys(mapping).find(e => ua.includes(e));
+  if (match) {
+    let [e, s] = mapping[match];
+    s ? (entitlementData.product_identifier = s, obj.subscriber.subscriptions[s] = subscriptionData) : obj.subscriber.subscriptions["pro_1y"] = subscriptionData, obj.subscriber.entitlements[e] = entitlementData;
+  } else {
+    obj.subscriber.subscriptions["pro_1y"] = subscriptionData;
+    obj.subscriber.entitlements.pro = entitlementData;
+  }
+
+  $done({
+    body: JSON.stringify(obj)
+  });
+}
